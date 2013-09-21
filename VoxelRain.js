378c8e5b9c95cf06a3152bdc1c1c0dff06308f44
ahoran3 <a_horan3@knights.ucf.edu>
2013-09-21 12:44:28 -0400

    personal update

--- VoxelRain.js
+++ VoxelRain.js
@@ -36,7 +36,7 @@ function addMessage(message){
 function menuHandler(){
 	var m = document.getElementById("menu");
 	modelId = m.selectedIndex;
-	addMessage(m.options[modelId].text);
+	addMessage("Selected index " + m.selectedIndex+ " :" + m.options[modelId].text);
 }
 
 function getObject(){
@@ -70,20 +70,20 @@ function mainFunction(){
 // How many objects along x, y and z
 var fallingObjects = new Array();
 var staticObjects = new Array();
-var cubeGrid = [6,2,6]; //number of objects in each direction
+var cubeGrid = [9,10,9]; //number of objects in each direction
 var angle = 0;
-var startHeight = 500;
+var startHeight = 200;
 var camera = null;
 var projMatrix = null;
 var center = null;
 var delta = null;
 var modelbounds = null;
-var fallSpeed = 0.01;
+var fallSpeed = 0.1;
 
 
 function draw(){
 	getObject();
-	
+
 		//get ready to make new cube or decrement CubeCounter
 	if(generationTime == 0){
 		makeCube = true;
@@ -107,13 +107,13 @@ function draw(){
 		var newXval = rand;
 		//assign number to new cube's x coordinate
 		randomNumber(0,9,0);
-		var newYval = rand;
+		var newZval = rand;
 		//assign rand to new cubes' z coordinate
 		//use push and shift on the arrays to add new cubes 
 		//and remove cubes that should no longer be falling 
 		//into the static cube array.
 		fallingObjects.push(model);
-		addMessage("New model added to canvas with coordinates (" + newXval+ ","+startHeight+"," + newYval +"). \nNumber of models in fallingObjects = " + fallingObjects.length + ". \nNumber of staticObjects = " + staticObjects.length+"\n");
+		addMessage("New model added to canvas with coordinates (" + newXval+ ","+startHeight+"," + newZval +"). \nNumber of models in fallingObjects = " + fallingObjects.length + ". \nNumber of staticObjects = " + staticObjects.length+"\n");
 		//if(falling object's position lowest it can go) 
 		//move the object from the falling array to the static array
 		//staticObjects.push(fallingObjects.shift());
@@ -138,9 +138,10 @@ function draw(){
 	sceneBounds.min = [modelbounds.min[0],modelbounds.min[1],modelbounds.min[2]]; // clone
 	sceneBounds.max = [
 		modelbounds.min[0]+cubeGrid[0]*delta,
-	    modelbounds.min[1]+cubeGrid[1]*5,
-		modelbounds.min[2]+cubeGrid[2]*delta
+	  	modelbounds.min[1]+cubeGrid[1]*delta,
+	  	modelbounds.min[2]+cubeGrid[2]*delta
 	];
+	
 	camera = new Camera(gl,sceneBounds,[0,1,0]);
 	projMatrix = camera.getProjMatrix();
 
@@ -149,15 +150,16 @@ function draw(){
 	var fallingModelMatrix = new Matrix4();
 	var staticModelMatrix = new Matrix4();
 
-		fallingModelMatrix.setTranslate(1*delta, 2*delta, 2*delta) // todo - scenebounds vs height array
-				           .translate(center[0], ((2*delta) > sceneBounds.min[1]) ? startHeight*fallSpeed:0, center[2])
-						   .rotate(1,0,1,1)
-						   .translate(-center[0], ((2*delta) > sceneBounds.min[1]) ? startHeight*fallSpeed:0, -center[2]);
-		staticModelMatrix.setTranslate(1*delta, 2*delta, 2*delta) // todo - scenebounds vs height array
-				           .translate(center[0], ((2*delta) > sceneBounds.min[1]) ? startHeight*fallSpeed:0, center[2])
-						   .rotate(1,0,1,1)
-						   .translate(-center[0], ((2*delta) > sceneBounds.min[1]) ? startHeight*fallSpeed:0, -center[2]);
+	fallingModelMatrix.setTranslate(1*delta, 2*delta, 1*delta) // todo - scenebounds vs height array -- explain
+			           .translate(center[0], ((2*delta) > sceneBounds.min[1]) ? startHeight*fallSpeed:0, center[2])
+					   .rotate(1,0,1,1)
+					   .translate(-center[0], ((2*delta) > sceneBounds.min[1]) ? startHeight*fallSpeed:0, -center[2]);
 	
+	staticModelMatrix.setTranslate(1*delta, 2*delta, 2*delta) // todo - scenebounds vs height array --explain
+			           .translate(center[0], 0, center[2])
+					   .rotate(1,0,1,1)
+					   .translate(-center[0], 0, -center[2]);
+
 	//draw all the falling objects
 	for(var f = 0; f < fallingObjects.length; f++){
 				fallingObjects[f].draw(projMatrix, viewMatrix, fallingModelMatrix);
@@ -168,20 +170,11 @@ function draw(){
 				staticObjects[s].draw(projMatrix, viewMatrix, staticModelMatrix);
 	}
 
-	// for (var z=0; z<cubeGrid[2]; z++)
-	// 	for (var y=0; y<cubeGrid[1]; y++)
-	// 		for (var x=0; x<cubeGrid[0]; x++){
-	// 			fallingModelMatrix.setTranslate(x*delta, y*delta, z*delta)
-	// 			           .translate(center[0],center[1],center[2])
-	// 					   .rotate(1,0,1,1)
-	// 					   .translate(-center[0],height*-center[1],-center[2]);
-	// 			model.draw(projMatrix, viewMatrix, fallingModelMatrix);
-	// 		}
 	angle++;
 	if (angle > 360) angle -= 360;
-	if (startHeight > 0) startHeight--; // todo - this should be an array?
+	if (startHeight > 0) startHeight--; // todo - this should be an array? --explain
 	window.requestAnimationFrame(draw);
-		
+
 }//end draw
 
 gl.clearColor(0,0,0,1);
